4690fbf60f6d89a56e61cdfbd3c5cbfe860797b8
MasayaDake <noimpslmtbrk@gmail.com>
2023-05-11 23:59:42 +0900

    演習課題の前までを対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # root "static_pages#home"
   # get  "static_pages/home"
   # get  "static_pages/help"
@@ -8,4 +9,5 @@ Rails.application.routes.draw do
   get  "/help",    to: "static_pages#help"
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
+  get  "/signup",  to: "users#new"
 end
