fa8675a83f1c8be76cbe6f85c7c48f69a1477b04
MasayaDake <noimpslmtbrk@gmail.com>
2023-05-15 03:22:46 +0900

    7章の対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
   def new
+    @user = User.new
+  end
+  def create
+    # @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    # Strong Parameter対応
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
