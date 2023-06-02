df257a320269a2681d69ef1411c94916be62270d
MasayaDake <noimpslmtbrk@gmail.com>
2023-06-02 22:15:23 +0900

    13.3までを対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,4 +78,25 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_url, status: :see_other) unless current_user.admin?
   end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  # beforeフィルター
+
+  # 正しいユーザーかどうかを確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
+
+  # 管理者かどうかを確認
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
+  end
+
 end
